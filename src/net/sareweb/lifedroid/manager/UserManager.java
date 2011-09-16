37eb9269a48f71c31ed8356aba262b42d27e6bd3
Aritz Galdos <galdos.aritz@gmail.com>
2011-09-16 14:24:51 +0200

    Added some methods to user Manager

--- src/net/sareweb/lifedroid/manager/UserManager.java
+++ src/net/sareweb/lifedroid/manager/UserManager.java
@@ -9,6 +9,15 @@ public class UserManager extends GenericManager {
 		_serviceClassName = "com.liferay.portal.service.UserServiceUtil";
 	}
 
+	public String getUserByEmailAddress(String companyId, String emailAddress) {
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("companyId", companyId);
+		params.put("emailAddress", emailAddress);
+
+		return _rClient.runGET(_serviceClassName,
+				_METHOD_NAME_GET_USER_BY_EMAIL_ADDRESS, params);
+	}
+
 	public String getUserById(String userId) {
 
 		HashMap<String, String> params = new HashMap<String, String>();
@@ -18,6 +27,17 @@ public class UserManager extends GenericManager {
 				params);
 	}
 
+	public String getUserByScreenName(String companyId, String screenName) {
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("companyId", companyId);
+		params.put("screenName", screenName);
+
+		return _rClient.runGET(_serviceClassName,
+				_METHOD_NAME_GET_USER_BY_SCREEN_NAME, params);
+	}
+
+	private String _METHOD_NAME_GET_USER_BY_EMAIL_ADDRESS = "getUserByEmailAddress";
 	private String _METHOD_NAME_GET_USER_BY_ID = "getUserById";
+	private String _METHOD_NAME_GET_USER_BY_SCREEN_NAME = "getUserByScreenName";
 
 }
