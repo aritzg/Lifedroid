110875c9b83403de835554ae47dcade7c1a5e306
Aritz Galdos <galdos.aritz@gmail.com>
2012-02-24 17:01:54 +0100

    StringUtils import error

--- src/net/sareweb/lifedroid/util/json/GenericParser.java
+++ src/net/sareweb/lifedroid/util/json/GenericParser.java
@@ -3,10 +3,9 @@ package net.sareweb.lifedroid.util.json;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import com.sun.xml.internal.ws.util.StringUtils;
 
 import android.util.Log;
 
